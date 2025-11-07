b153cda18f6bbcc06a2974b17ef384b8aeccf368
2mer <kenyweb7@gmail.com>
2025-11-07 19:35:01 +0200

    rename base

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/oh-my-context/",
+	base: "/hold-my-context/",
 });
